9c433904b6c8f2a11f907130f536a5c1ec15fb3b
jaume2000 <jaumeig95@gmail.com>
2023-04-02 22:07:23 +0200

    Añadido gitignore

---
+++ part1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './message.js'
+
+const App = () => {
+  return (
+    <div>
+      <Message message= "mundo"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/message.js
@@ -0,0 +1,9 @@
+const Message = (params) => {
+    return(
+        <div>
+            <h1>Hola mundo: {params.message}</h1>
+        </div>
+    )
+}
+
+export default Message;
